72e6cde4e88076d630e6a3d87b1d77a0a5e2bdec
CianORuanaidh <cianoruanaidh_dev@Cians-MacBook-Pro.local>
2020-10-15 09:05:03 -0400

    Updates to site - scrolling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
-function App() {
+function AppTwo() {
   return (
     <div>
       {/* <video autoPlay muted loop id="myVideo">
@@ -21,7 +21,7 @@ function App() {
 
       <Router>
         <HeadNavigation></HeadNavigation>
-        <main className="main-body">
+        <main className="main-body" onScroll={this.handleScroll}>
           <Route path="/" exact component={HeroSection}></Route>          
           <Route path="/about" component={AboutMe}></Route>
           <Route path="/resume" component={MyResume}></Route>
@@ -31,5 +31,55 @@ function App() {
     </div>
   );
 }
+
+
+
+class App extends React.Component {
+  constructor() {
+      super();
+      this.state = {};
+    }
+
+  componentDidMount() {
+    const height = document.getElementById('main-main').clientHeight;
+    this.setState({ height });
+  }
+
+  handleScroll = (event) => {
+    console.log('SCROLL')
+    console.log('innerHeight: ', window.innerHeight)
+    console.log('scrollY: ', window.scrollY)
+    console.log('ROOT HEIGHT: ', this.state.height)
+    // console.log(window.outerHeight)
+    // console.log(event)
+    // var heightBound = window.height * 0.8
+    // if (heightBound > window.scrollY) {
+    //     // Probably you want to load new cards?
+    //     this.props.getCards(...);
+    // } 
+  }
+
+  handleWheel(event) {
+    console.log('WHEEL')
+  }
+
+    
+  render() {
+      return (
+        <div>
+          <Router>
+            <HeadNavigation></HeadNavigation>
+            <main id="main-main"className="main-body" onScroll={this.handleScroll} onWheel={this.handleScroll}>
+              <Route path="/" exact component={HeroSection}></Route>          
+              <Route path="/about" component={AboutMe}></Route>
+              <Route path="/resume" component={MyResume}></Route>
+              <Route path="/contact" component={Contact}></Route>
+            </main>
+          </Router>
+        </div>
+      );
+  }
+}
+
 
 export default App;
