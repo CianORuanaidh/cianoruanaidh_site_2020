a6f37f3a9168ab1007d0ed92bcfd87dd2f3867b2
CianORuanaidh <cianoruanaidh_dev@Cians-MacBook-Pro.local>
2020-10-29 21:54:24 -0400

    Nav updates

--- src/components/HeadNavigation.js
+++ src/components/HeadNavigation.js
@@ -23,15 +23,12 @@ class HeadNavigation extends React.Component {
         return(
             <header>
                 <nav className={this.state.toggle ? 'show-overlay' : 'hide-overlay'}>    
-                    <ul>
-                        {/* <li>
-                            <button className="hamburger-link">
-                            <input type="checkbox" className="toggler" />
-                            <div className="hamburger"><div></div></div>
-                            </button>
-                        </li> */}
-                        {/* <h1>{this.state.toggle ? 'hi' : 'ho'}hhhhh</h1> */}
-                        <li className="menu" onClick={this.onToggle}><Link to="/">close</Link></li>
+                    {/* <button className="menu" onClick={this.onToggle}><Link to="/">{!this.state.toggle ? 'show' : 'close'}</Link></button> */}
+                    <button className="hamburger-link menu" onClick={this.onToggle}>
+                    <input type="checkbox" className="toggler" />
+                    <div className="hamburger"><div></div></div>
+                    </button>
+                    <ul className="link-list">
                         <li className="link"><Link to="/about">about</Link></li>
                         <li className="link"><Link to="/resume">resume</Link></li>
                         <li className="link"><Link to="/portfolio">portfolio</Link></li>
