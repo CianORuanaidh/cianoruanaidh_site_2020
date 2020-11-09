456aec1fd65820e6c613cade11a93c85b9ea9220
CianORuanaidh <cianoruanaidh_dev@Cians-MacBook-Pro.local>
2020-11-08 19:46:31 -0500

    Updates

---
+++ precache-manifest.d06cf9b9ab5b3033996b540b9be8c0bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed20e4891fc8530af7e8fbab381401cb",
+    "url": "/cianoruanaidh_site_2020/index.html"
+  },
+  {
+    "revision": "790bdc087c7695ed4651",
+    "url": "/cianoruanaidh_site_2020/static/css/main.767c705b.chunk.css"
+  },
+  {
+    "revision": "ca1d7bf76449aecdc238",
+    "url": "/cianoruanaidh_site_2020/static/js/2.0604a245.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/cianoruanaidh_site_2020/static/js/2.0604a245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790bdc087c7695ed4651",
+    "url": "/cianoruanaidh_site_2020/static/js/main.98ce002c.chunk.js"
+  },
+  {
+    "revision": "edcf5634597a4a18fcf7",
+    "url": "/cianoruanaidh_site_2020/static/js/runtime-main.1d3e3fb0.js"
+  },
+  {
+    "revision": "3bb31c6dba240c645be6e40ae60055e5",
+    "url": "/cianoruanaidh_site_2020/static/media/assets-a-single-word-screen.3bb31c6d.png"
+  },
+  {
+    "revision": "b6066b4973fcef658fd7df3fab3360c8",
+    "url": "/cianoruanaidh_site_2020/static/media/assets-click-bait-mock-up-screen.b6066b49.png"
+  },
+  {
+    "revision": "5e8dca6b4ef18b06a099ae3ef5d1757d",
+    "url": "/cianoruanaidh_site_2020/static/media/assets-toronto-bike-share-app-screen.5e8dca6b.png"
+  },
+  {
+    "revision": "d05d32d0fad97cf69d86c64800d1187a",
+    "url": "/cianoruanaidh_site_2020/static/media/cian_oruanaidh_resume_2020.d05d32d0.pdf"
+  }
+]);
